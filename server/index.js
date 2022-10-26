5f0ab0d70dd55f99fe2075d7aa45cf6c668b651a
Heshan <mheshan27@gmail.com>
2022-10-26 17:04:50 +0530

    chore-setup server
    setup initial express server

--- server/index.js
+++ server/index.js
@@ -3,3 +3,7 @@ const app = express();
 
 require("dotenv").config();
 require("express-async-errors");
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${process.env.PORT}`);
+});
