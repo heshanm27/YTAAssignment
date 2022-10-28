2dfbb1899895304bac2d6f50ab1662bfda25e047
Heshan <mheshan27@gmail.com>
2022-10-28 10:52:05 +0530

    refactor : app port variable

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ const errorHadnler = require("./middleware/error_handle");
 //Router module imports
 const authorRoute = require("./routes/authorRoute");
 const bookRoute = require("./routes/bookRoute");
+const { METHOD_FAILURE } = require("http-status-codes");
 
 // //route paths
 app.use("/api/v1/author", authorRoute);
@@ -36,15 +37,15 @@ app.use(notFound);
 app.use(errorHadnler);
 
 //If PORT environment Variable Not avaliable use Port 5000
-const port = process.env.PORT || 5000;
 
+app.set("port", process.env.PORT || 5000);
 /**
  * @description start express server and start Mongodb atlas connection
  */
 const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => {
+    app.listen(app.get("port"), () => {
       console.log(`Server listening on port ${process.env.PORT}`);
     });
   } catch (err) {
@@ -54,3 +55,5 @@ const startServer = async () => {
 
 //call to start the server
 startServer();
+
+module.exports = app;
