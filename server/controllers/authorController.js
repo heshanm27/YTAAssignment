93ba0759a40837ba3c0e60403d090abad175e12a
Heshan <mheshan27@gmail.com>
2022-10-28 09:18:28 +0530

    refactor:Author api controller

--- server/controllers/authorController.js
+++ server/controllers/authorController.js
@@ -1,19 +1,36 @@
 const authorModel = require("../models/authorModel");
-const { CustomAPIError, BadRequestError } = require("../errors/errorClass");
+const {
+  CustomAPIError,
+  NotFoundError,
+  BadRequestError,
+} = require("../errors/errorClass");
+
+/**
+ *  Get All the authors
+ *  @route GET /api/v1/author
+ * */
 const getAllAuthorDetails = async (req, res) => {
   const authors = await authorModel.find({});
   res.status(200).json({ authors });
 };
 
+/**
+ * Get author details by id
+ * @route GET /api/v1/author/:id
+ * */
 const getAuthorDetailsByID = async (req, res) => {
   const author = await authorModel.findById(req.params.id);
   if (author) {
     res.status(200).json({ msg: "Author found", author });
   } else {
-    throw new CustomAPIError("Author not found.", 404);
+    throw new NotFoundError("Author not found.");
   }
 };
 
+/**
+ * Create new author
+ * @route POST /api/v1/author
+ * */
 const postAuthorDetails = async (req, res) => {
   try {
     const author = await authorModel.create(req.body);
@@ -25,6 +42,10 @@ const postAuthorDetails = async (req, res) => {
   }
 };
 
+/**
+ * Update author details
+ * @route PUT /api/v1/author/:id
+ */
 const updateAuthorDetails = async (req, res) => {
   try {
     const author = await authorModel.findByIdAndUpdate(
